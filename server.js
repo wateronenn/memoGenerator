b9e1543f100434cf81062ce38db6c3b5f7ebc05a
wateronenn <anamneung@gmail.com>
2026-06-10 17:04:01 +0700

    add css

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname)));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'memo_generator.html'));
